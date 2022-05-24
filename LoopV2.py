f815723d760f700fe9f433e31fe22f45fbc4823c
Lazaro Casas <lcasasan@cisco.com>
2022-05-24 13:50:59 -0500

    Loop option 3

--- LoopV2.py
+++ LoopV2.py
@@ -1,4 +1,4 @@
 ages = {"Lazaro": 37, "Monica": 36, "Romina": 1}
 list(ages.items())
-for age in ages.items():
-	print(age)
+for age, quantity in ages.items():
+	print("The age of {} {}.".format(age, quantity))
